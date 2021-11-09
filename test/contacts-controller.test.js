1f8a6561b4703350482c7320a85876681faae03f
Igor Mokhnatskyy <55652900+gigorko1993@users.noreply.github.com>
2021-11-09 12:54:35 +0200

    add test to UpdateContact

--- test/contacts-controller.test.js
+++ test/contacts-controller.test.js
@@ -1,16 +1,36 @@
 const { updateContact } = require("../controllers/contacts");
-const Contacts = require("../repository/contacts");
+const Contact = require("../repository/contacts");
 
 jest.mock("../repository/contacts");
 
 describe("Unit test controller updateContact", () => {
   const req = { params: { id: 2 }, body: {}, user: { _id: 1 } };
-  const res = {};
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn((data) => data),
+  };
   const next = jest.fn();
 
-  it("Contact exist", () => {
-    const result = updateContact(req, res, next);
+  beforeEach(() => {
+    Contact.updateContact = jest.fn();
   });
 
-  it("Contact does not exist", () => {});
+  it("Contact exist", async () => {
+    const contact = { id: 2, name: "ddd" };
+    Contact.updateContact = jest.fn(() => {
+      return contact;
+    });
+    const result = await updateContact(req, res, next);
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty("status");
+    expect(result).toHaveProperty("code");
+    expect(result).toHaveProperty("data");
+    expect(result.data.contact).toEqual(contact);
+  });
+
+  it("Contact does not exist", async () => {
+    await expect(updateContact(req, res, next)).rejects.toEqual({
+      message: "Not Found",
+    });
+  });
 });
